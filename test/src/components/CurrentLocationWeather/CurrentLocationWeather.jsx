5b6c736e4e74c745ff29aca23bf3bace911dd07e
Arfaleas <vangsnesk@gmail.com>
2023-09-14 16:38:47 +0200

    added mainpage and location bar with weather

---
+++ test/src/components/CurrentLocationWeather/CurrentLocationWeather.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import "./index.css"
+import { FaTemperatureFull, FaUmbrella } from "react-icons/fa6"
+import { SiWindicss } from "react-icons/si"
+
+const iconURL = "https://openweathermap.org/img/wn/"
+
+const CurrentLocationWeather = ({ weather }) => {
+  console.log(weather)
+  return (
+    <div className="container">
+      {weather ? (
+        <>
+          <img
+            className="weather-icon"
+            src={`${iconURL}${weather.weather[0].icon}@2x.png`}
+            alt="weather-icon"
+          />
+          <FaTemperatureFull size={"20px"} />
+          <div className="temperature">{weather.main.temp.toFixed(1)}&deg;</div>
+          <div className="feels-like">
+            Feels like
+            <span className="temperature">
+              {weather.main.feels_like.toFixed(1)}&deg;
+            </span>
+          </div>
+          <FaUmbrella size={"20px"} />
+          <div className="rain">
+            {weather.rain === undefined ? 0 : weather.rain} mm
+          </div>
+          <div className="wind">
+            <SiWindicss size={"20px"} style={{ margin: "4px" }} />
+            {Math.ceil(weather.wind.speed)}
+            <span style={{ fontSize: "13px" }}>
+              {weather.wind.gust === undefined
+                ? null
+                : Math.ceil(weather.wind.gust)}
+            </span>
+            <span style={{ fontSize: "11px", marginLeft: "2px" }}>m/s</span>
+          </div>
+        </>
+      ) : null}
+    </div>
+  )
+}
+
+export default CurrentLocationWeather
---
+++ test/src/components/Hero.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import Search from "./Search"
+import axios from "axios"
+import CurrentLocationWeather from "./CurrentLocationWeather/CurrentLocationWeather"
+
+const Hero = () => {
+  const [location, setLocation] = useState(null)
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error)
+    } else {
+      console.log("Geolocation not supported")
+    }
+  }, [])
+
+  function success(position) {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    setLocation({ longitude, latitude })
+
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_API_BASE_URL
+        }?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_API_KEY
+        }&units=metric`
+      )
+      .then(function (response) {
+        setWeather(response.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
+  function error() {
+    console.log("Unable to retrieve your location")
+  }
+
+  return (
+    <div className="main-container">
+      <CurrentLocationWeather weather={weather} />
+      <Search />
+    </div>
+  )
+}
+export default Hero
