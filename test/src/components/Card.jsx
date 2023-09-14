26dbfbe01b36e82058d70ab97d18ac13c8fa04b5
Arfaleas <vangsnesk@gmail.com>
2023-09-14 10:33:53 +0200

    Added more details and css

--- test/src/components/Card.jsx
+++ test/src/components/Card.jsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/prop-types */
 const iconURL = "https://openweathermap.org/img/wn/"
-const Card = ({ city: { name, weather } }) => {
+const Card = ({ city }) => {
+  console.log(city)
   return (
     <div className="card-container">
-      <h1 className="card-title">Weather in</h1>
-      <h2 className="card-city">{name}</h2>
-      <img
-        className="card-icon"
-        src={`${iconURL}${weather[0].icon}@2x.png`}
-        alt="weather-icon"
-      />
-      <p className="card-weather">{weather[0].description}</p>
+      <h1 className="card-title">Weather in:</h1>
+      <h1 className="card-city">{city.name}</h1>
+      <div className="card-icon-container">
+        <img
+          className="card-icon"
+          src={`${iconURL}${city.weather[0].icon}@2x.png`}
+          alt="weather-icon"
+        />
+        <p className="card-weather-type">{city.weather[0].main}</p>
+      </div>
+      <p className="card-temperature">
+        Temperature: {city.main.temp.toFixed(1)}
+      </p>
+      <p className="card-feels-like">
+        Feels like: {city.main.feels_like.toFixed(1)}
+      </p>
+      <p className="card-weather">{}</p>
     </div>
   )
 }
