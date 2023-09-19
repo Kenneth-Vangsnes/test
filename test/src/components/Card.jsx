0ceebd51cd2dbf5a97a2a03e90aac71b2f3ecbb6
Arfaleas <vangsnesk@gmail.com>
2023-09-19 15:05:59 +0200

    Added symbols, and rain to card along with css

--- test/src/components/Card.jsx
+++ test/src/components/Card.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 import { FiArrowUp } from "react-icons/fi"
+import { FaTemperatureFull, FaUmbrella } from "react-icons/fa6"
+import { SiWindicss } from "react-icons/si"
 
 const iconURL = "https://openweathermap.org/img/wn/"
 const Card = ({ city }) => {
@@ -14,16 +16,28 @@ const Card = ({ city }) => {
         />
         <p className="card-weather-type">{city.weather[0].main}</p>
       </div>
-      <p className="card-temperature">{city.main.temp.toFixed(1)}&deg;</p>
+      <div className="card-temperature-div">
+        <FaTemperatureFull size={"20px"} />
+        <p className="card-temperature">{city.main.temp.toFixed(1)}&deg;</p>
+      </div>
       <p className="card-feels-like">
         Feels like
         <span style={{ color: "red", marginLeft: "2px" }}>
           {city.main.feels_like.toFixed(1)}&deg;
         </span>
       </p>
-      <p className="card-wind">{Math.ceil(city.wind.speed)} m/s</p>
-
-      <FiArrowUp style={{ transform: `rotate(${city.wind.deg})` }} />
+      <div className="card-rain">
+        <FaUmbrella size={"20px"} />
+        <span className="card-rain-amount">{city.rain ? city.rain : 0} mm</span>
+      </div>
+      <div className="card-wind-div">
+        <SiWindicss size={"20px"} />
+        <p className="card-wind">{Math.ceil(city.wind.speed)} m/s</p>
+        <FiArrowUp
+          size={"20px"}
+          style={{ transform: `rotate(${city.wind.deg})` }}
+        />
+      </div>
     </div>
   )
 }
