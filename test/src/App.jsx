b3e3138cf757df7f12861e18683735e871d5e3e1
Arfaleas <vangsnesk@gmail.com>
2023-09-19 12:39:21 +0200

    testing bug fix for onscreen keyboard

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react"
 import "./App.css"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
+  useEffect(() => {
+    const handleResize = () => {
+      const metaViewport = document.querySelector("meta[name=viewport]")
+      metaViewport.setAttribute(
+        "content",
+        "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      )
+      console.log("hey")
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
   return (
     <>
       <Navbar />
