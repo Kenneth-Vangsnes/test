eb5311323db44a7eeb55c746f003bb7726512f2a
Arfaleas <vangsnesk@gmail.com>
2023-09-19 13:03:08 +0200

    reverted commits

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -3,23 +3,6 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
-  useEffect(() => {
-    const handleResize = () => {
-      const metaViewport = document.querySelector("meta[name=viewport]")
-      metaViewport.setAttribute(
-        "content",
-        "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-      )
-      console.log("hey")
-    }
-
-    window.addEventListener("resize", handleResize)
-
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [])
-
   return (
     <>
       <Navbar />
