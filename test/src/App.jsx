adb63f5cfeebfb6075da1ed1c91abd31a804170e
Arfaleas <vangsnesk@gmail.com>
2023-09-19 12:51:04 +0200

    bugfix #2 on keyboard issue

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -6,12 +6,14 @@ import Navbar from "./components/Navbar"
 function App() {
   useEffect(() => {
     const handleResize = () => {
+      const initialHeight = window.innerHeight
+
+      document.documentElement.style.setProperty("overflow", "auto")
       const metaViewport = document.querySelector("meta[name=viewport]")
       metaViewport.setAttribute(
         "content",
-        "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        "height=" + initialHeight + "px, width=device-width, initial-scale=1.0"
       )
-      console.log("hey")
     }
 
     window.addEventListener("resize", handleResize)
